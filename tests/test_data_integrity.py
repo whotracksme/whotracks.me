26cc3a3c18519ef30fd1205620f76a32cdb8d505
Sam Macbeth <sammacbeth@users.noreply.github.com>
2017-11-22 13:19:57 +0100

    Add travis CI testing (#12)
    * Add travis CI testing
    
    * Add __init__.py for db module
    
    * Move db validity test to tests dir
    
    * Add tests module init.py
    
    * Fix relative paths in tests

--- tests/test_data_integrity.py
+++ tests/test_data_integrity.py
@@ -9,7 +9,7 @@ class TestDataIntegrity(unittest.TestCase):
         self.conn = load_tracker_db()
 
     def test_all_trackers_have_db_entry(self):
-        apps = json.load(open('../data/apps.json'))
+        apps = json.load(open('./data/apps.json'))
         cur = self.conn.cursor()
         app_ids = sorted(apps.keys())
         db_trackers = cur.execute('select id, category_id from trackers where id IN ({}) order by id'.format(
@@ -17,11 +17,11 @@ class TestDataIntegrity(unittest.TestCase):
         )).fetchall()
         self.assertEqual(set(app_ids), set(map(itemgetter(0), db_trackers)))
         # check all have a category
-        without_category = filter(lambda tracker: tracker[1] is None, db_trackers)
+        without_category = list(filter(lambda tracker: tracker[1] is None, db_trackers))
         self.assertEqual([], without_category)
 
     def test_all_companies_have_db_entry(self):
-        companies = json.load(open('../data/companies.json'))
+        companies = json.load(open('./data/companies.json'))
         cur = self.conn.cursor()
         company_ids = sorted(companies.keys())
         db_companies = cur.execute('select id from companies where id IN ({}) order by id'.format(
