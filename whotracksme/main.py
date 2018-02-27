8dc3966932d6cb2bb865b94957e140623ab3f96e
ecnmst <ecnmst@users.noreply.github.com>
2018-02-27 15:08:38 +0100

    Issue fixes and QA utils (#57)
    * fixes for #53, #52, #51, #50, #49, #34
    
    * added right to ammend guide
    
    * one commit to cherry-pick them all
    
    * Remove tracker db entries.
    
    * qa utils
    
    * simpler way to provide absolute path to static for error pages

--- whotracksme/main.py
+++ whotracksme/main.py
@@ -8,7 +8,7 @@ Whotracks.me website development tool.
 Usage:
     whotracksme website [serve]
     whotracksme data [list]
-    whotracksme db (create|dump)
+    whotracksme db (create|dump|check_urls)
     whotracksme -h | --help
 
 Options:
@@ -20,9 +20,11 @@ Options:
 import docopt
 import os
 import sqlite3
+from pathlib import Path
 from whotracksme.website.builder import Builder
 from whotracksme.website.serve import serve
 from whotracksme.data.loader import load_tracker_db
+from whotracksme.qa.todo import upgrade_to_https, create_task_files
 
 
 class objectview:
@@ -58,11 +60,18 @@ def main():
         if args.create:
             load_tracker_db(loc='tracker.db')
         elif args.dump:
+            # TODO: Replace this with `pathlib`
             tracker_db_path = os.path.join(os.path.dirname(__file__), 'data', 'assets', 'trackerdb.sql')
             conn = sqlite3.connect('tracker.db')
             with open(tracker_db_path, 'w') as fp:
                 for line in conn.iterdump():
                     fp.write('%s\n' % line)
+        elif args.check_urls:
+            needqa = Path('needqa')
+            if not needqa.exists():
+                needqa.mkdir()
+            https_upgrade = upgrade_to_https(tracker_db='tracker.db')
+            create_task_files(needqa_folder=needqa, **https_upgrade)
 
 
 if __name__ == '__main__':
--- whotracksme/website/templates.py
+++ whotracksme/website/templates.py
@@ -6,6 +6,7 @@ import subprocess
 
 from jinja2 import Environment, FileSystemLoader, Markup
 import markdown
+from pathlib import Path
 
 from whotracksme.website.utils import print_progress
 from whotracksme.website.plotting.colors import (
@@ -33,6 +34,14 @@ def site_to_json(data_source, blog_posts):
             url=data_source.url_for("tracker", tracker_id),
             weight=(1.0 / tracker.get("rank", 1)) * 1000
         )
+        # Search a tracker by domain.
+        for d in data_source.trackers.get_domains(tracker_id):
+            submit_key(
+                name=d,
+                type="tracker",
+                url=data_source.url_for("tracker", tracker_id),
+                weight=(1.0 / tracker.get("rank", 1)) * 1000
+            )
 
     for (company_id, company) in data_source.companies.items():
         submit_key(
@@ -62,7 +71,7 @@ def site_to_json(data_source, blog_posts):
 
 
 # Paths needed for generating urls
-SITE_PATH = os.path.abspath('_site')
+SITE_PATH = Path('_site').absolute().as_posix()
 PATHS = {
     "_site": SITE_PATH,
     "static": '/static',
@@ -91,14 +100,14 @@ CATEGORY_DESC = {
 
 def copy_custom_error_pages(data):
     error_pages = {
-        "not-found": get_template(data, "not-found.html"),
-        "tracker-not-found": get_template(data, "tracker-not-found.html"),
-        "website-not-found": get_template(data, "website-not-found.html")
+        "not-found": get_template(data, "not-found.html",  path_to_root=''),
+        "tracker-not-found": get_template(data, "tracker-not-found.html", path_to_root=''),
+        "website-not-found": get_template(data, "website-not-found.html", path_to_root='')
     }
 
     for error, template in error_pages.items():
         with open('_site/{}.html'.format(error), 'w') as output:
-            output.write(render_template(template=template))
+            output.write(render_template(template=template, path_to_root=''))
 
 
 def generate_sitemap(data, blog_posts):
@@ -127,10 +136,13 @@ def get_template(data_source, name, render_markdown=False, path_to_root='.'):
             ]
         )
         env.filters["markdown"] = lambda text: Markup(md.convert(text))
-    env.filters["prettify_label"] = lambda text: text.replace("_", " ").capitalize() if text not in [None, "None", ""] else ""
+    env.filters["prettify_label"] = \
+        lambda text: " ".join(map(lambda x: x.capitalize(), text.split("_"))) \
+        if text not in [None, "None", ""] else ""
     env.filters["normalize_domain_name"] = lambda text: text.replace("www.", "")
     env.filters["absolute_og_urls"] = lambda url: url.replace("../", "").replace("./", "")
-    env.filters["url_for"] = lambda entity, id: data_source.url_for(entity, id, path_to_root=path_to_root)
+    env.filters["url_for"] = \
+        lambda entity, id: data_source.url_for(entity, id, path_to_root=path_to_root)
     env.filters["get_app_name"] = lambda id: data_source.trackers.get_name(id)
     env.filters["get_company_name"] = lambda id: data_source.get_company_name(id)
     env.filters["get_site_name"] = lambda id: data_source.sites.get_name(id)
@@ -174,28 +186,37 @@ def create_site_structure(static_path):
 
     Returns: rendered site ready to publish (_site)
     """
-    if not os.path.exists('data'):
-        os.mkdir('data')
-    if not os.path.exists(PATHS.get('_site')):
-        os.mkdir(PATHS.get("_site"))
-    if not os.path.exists(os.path.join(SITE_PATH, PATHS.get("trackers")[1:])):
-        os.mkdir(os.path.join(SITE_PATH, PATHS.get("trackers")[1:]))
-    if not os.path.exists(os.path.join(SITE_PATH, PATHS.get("websites")[1:])):
-        os.mkdir(os.path.join(SITE_PATH, PATHS.get("websites")[1:]))
-    if not os.path.exists(os.path.join(SITE_PATH, PATHS.get("companies")[1:])):
-        os.mkdir(os.path.join(SITE_PATH, PATHS.get("companies")[1:]))
-    if not os.path.exists(os.path.join(SITE_PATH, PATHS.get("blog")[1:])):
-        os.mkdir(os.path.join(SITE_PATH, PATHS.get("blog")[1:]))
+    def ensure_dir_exists(entry):
+        if not entry.exists():
+            entry.mkdir()
+
+    # data = Path("data")
+    # ensure_dir_exists(data)
+
+    _site = Path(PATHS.get("_site"))
+    ensure_dir_exists(_site)
+
+    trackers = _site.joinpath('trackers')
+    ensure_dir_exists(trackers)
+
+    websites = _site.joinpath('websites')
+    ensure_dir_exists(websites)
+
+    companies = _site.joinpath('companies')
+    ensure_dir_exists(companies)
+
+    blog = _site.joinpath('blog')
+    ensure_dir_exists(blog)
 
     # Copy static folder inside _site/ (if not there already)
-    _site_static = os.path.join(SITE_PATH, "static")
-    if os.path.exists(_site_static):
-        shutil.rmtree(_site_static)
-        shutil.copytree(static_path, _site_static)
-    elif not os.path.exists(_site_static):
-        shutil.copytree(static_path, _site_static)
+    _site_static = _site.joinpath('static')
+    if _site_static.exists():
+        shutil.rmtree(_site_static.as_posix())
+        shutil.copytree(static_path, _site_static.as_posix())
+    elif not _site_static.exists():
+        shutil.copytree(static_path, _site_static.as_posix())
 
     # compile static/scss directly to _site/static/css
-    scss_folder = os.path.join(static_path, "scss")
-    css_folder = os.path.join("_site/static", "css")
+    scss_folder = Path(static_path).joinpath("scss")
+    css_folder = _site_static.joinpath("css")
     compile_scss_to_css(scss_folder, css_folder)
