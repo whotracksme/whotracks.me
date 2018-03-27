59ac18b8f2245193e89a5b0f78235e8233df1a68
Sam Macbeth <sam@cliqz.com>
2018-03-27 17:00:13 +0200

    Fix tests

--- tests/test_data_integrity.py
+++ tests/test_data_integrity.py
@@ -23,13 +23,18 @@ class TestDataIntegrity(unittest.TestCase):
         self.assertEqual([], without_category)
 
     def test_all_companies_have_db_entry(self):
-        companies = self.ds.trackers.companies
+        companies = self.ds.companies
         cur = self.conn.cursor()
         company_ids = sorted(companies.get_snapshot().company)
+
+        # company_id can be a company id or tracker id
         db_companies = cur.execute('select id from companies where id IN ({}) order by id'.format(
             ','.join(["'{}'".format(id) for id in company_ids])
         )).fetchall()
-        self.assertEqual(set(company_ids), set(map(itemgetter(0), db_companies)))
+        db_trackers = cur.execute('select id, category_id from trackers where id IN ({}) order by id'.format(
+            ','.join(["'{}'".format(id) for id in company_ids])
+        )).fetchall()
+        self.assertEqual(set(company_ids), set(map(itemgetter(0), db_companies)) | set(map(itemgetter(0), db_trackers)))
 
 
 if __name__ == '__main__':
