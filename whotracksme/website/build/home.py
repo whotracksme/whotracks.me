cbd8ae0f0ca5ebd72fdd0d3e0f2344598bc21041
ecnmst <aldo@cliqz.com>
2018-05-14 10:58:43 +0200

    last 3  posts in homepage

--- whotracksme/website/build/home.py
+++ whotracksme/website/build/home.py
@@ -4,11 +4,14 @@ from whotracksme.website.plotting.companies import overview_bars
 from whotracksme.website.build.companies import company_reach
 from whotracksme.website.utils import print_progress
 from whotracksme.website.templates import get_template, render_template
+from whotracksme.website.build.blog import load_blog_posts
 
 
 def build_home(data):
     top10 = company_reach(data.companies)
     header_graph = Markup(overview_bars(top10))
+
+    posts = load_blog_posts()[:3]
 
     with open('_site/index.html', 'w') as output:
         output.write(render_template(
@@ -20,7 +23,8 @@ def build_home(data):
             least_tracked_sites=data.sites.sort_by(metric='trackers', descending=False)[:20],
             websites=data.sites.summary_stats(),
             tracker_stats=data.trackers.summary_stats(),
-            top10=top10
+            top10=top10,
+            posts=posts
         ))
 
     print_progress(text="Generate home page")
