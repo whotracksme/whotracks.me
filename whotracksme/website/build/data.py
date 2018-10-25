e1aaa994f2cceae64340b467a7332e6acf6fd268
Sam Macbeth <sam@cliqz.com>
2018-10-25 13:23:45 +0200

    Basic API for tracker overview stats.

--- whotracksme/website/build/data.py
+++ whotracksme/website/build/data.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 from whotracksme.data.db import load_tracker_db, create_tracker_map
 
 def build_tracker_db():
@@ -6,3 +7,20 @@ def build_tracker_db():
         db_map = create_tracker_map(load_tracker_db(), with_iab_vendors=True)
         db_map['about'] = 'WhoTracks.Me tracker database: whotracks.me'
         json.dump(db_map, output, indent=2, sort_keys=True)
+
+def build_api(data):
+    # tracker overviews
+    data_dir = Path('_site/data/trackers/global')
+    if not data_dir.exists():
+        data_dir.mkdir(parents=True)
+
+    for id, stats in data.trackers.iter():
+        stats = data.trackers.get_tracker(id)
+        stats['overview'] = dict(stats['overview'])
+        # drop some columns
+        for col in ['Index', 'companies', 'month', 'trackers', 'tracker', 'id', 'company_id', 'category', 'country']:
+            del stats['overview'][col]
+        stats['date_range'] = [date.strftime('%Y-%m') for date in stats['date_range']]
+        # print(stats)
+        with open(f'_site/data/trackers/global/{id}.json', 'w') as output:
+            json.dump(stats, output)
