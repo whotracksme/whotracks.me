7b5e61afd06da4405edbda0d8982513ce12b189f
Sam Macbeth <sam@cliqz.com>
2019-02-19 15:22:13 +0100

    Fix tests.

--- tests/test_sites_data.py
+++ tests/test_sites_data.py
@@ -5,9 +5,8 @@ from whotracksme.data import DataSource
 class TestSitesData(unittest.TestCase):
 
     def test_all_sites_have_category(self):
-        sites = ds = DataSource().sites.get_snapshot()
-        sites[sites.category == '']
-        no_category_sites = list(sites[sites.category == ''].itertuples())
+        sites = DataSource().sites.get_snapshot()
+        no_category_sites = list(filter(lambda s: s.category == '', sites))
         self.assertEqual(no_category_sites, [])
 
 
