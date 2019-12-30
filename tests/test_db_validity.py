491752c6058dfa28459531979ba29299b955d437
Sam Macbeth <sam@cliqz.com>
2019-12-30 12:51:35 +0100

    Dump new database

--- tests/test_db_validity.py
+++ tests/test_db_validity.py
@@ -38,5 +38,35 @@ class ValidateTrackerDatabase(unittest.TestCase):
         tracker_count = cur.fetchone()[0]
         self.assertEqual(domain_tracker_count, tracker_count)
 
+    def test_tracker_ids_ascii(self):
+        cur = self.conn.cursor()
+        cur.execute('SELECT id FROM trackers')
+        trackers = cur.fetchall()
+        for tracker in trackers:
+            try:
+                tracker[0].encode('ascii')
+            except:
+                self.fail(f'"{tracker[0]}" is not ascii')
+
+    def test_company_ids_ascii(self):
+        cur = self.conn.cursor()
+        cur.execute('SELECT id FROM companies')
+        companies = cur.fetchall()
+        for company in companies:
+            try:
+                company[0].encode('ascii')
+            except:
+                self.fail(f'"{company[0]}" is not ascii')
+
+    def test_domains_ascii(self):
+        cur = self.conn.cursor()
+        cur.execute('SELECT domain FROM tracker_domains')
+        domains = cur.fetchall()
+        for domain in domains:
+            try:
+                domain[0].encode('ascii')
+            except:
+                self.fail(f'"{domain[0]}" is not ascii')
+
 if __name__ == '__main__':
     unittest.main()
